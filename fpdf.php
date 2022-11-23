1c94f1a84f85897bc411935c9b3b7d126cdbcf28
Ameon <ameon@ya.ru>
2022-11-23 10:03:27 +0500

    update

--- fpdf.php
+++ fpdf.php
@@ -171,6 +171,102 @@ function __construct($orientation='P', $unit='mm', $size='A4')
 	$this->PDFVersion = '1.3';
 }
 
+// Create Table
+  function WriteTable($tcolums)
+  {
+    // go through all colums
+    for ($i = 0; $i < sizeof($tcolums); $i++)
+    {
+        $current_col = $tcolums[$i];
+        $height = 0;
+        
+        // get max height of current col
+        $nb=0;
+        for($b = 0; $b < sizeof($current_col); $b++)
+        {
+          // set style
+          $this->SetFont($current_col[$b]['font_name'], $current_col[$b]['font_style'], $current_col[$b]['font_size']);
+          $color = explode(",", $current_col[$b]['fillcolor']);
+          $this->SetFillColor($color[0], $color[1], $color[2]);
+          $color = explode(",", $current_col[$b]['textcolor']);
+          $this->SetTextColor($color[0], $color[1], $color[2]);            
+          $color = explode(",", $current_col[$b]['drawcolor']);            
+          $this->SetDrawColor($color[0], $color[1], $color[2]);
+          $this->SetLineWidth($current_col[$b]['linewidth']);
+                      
+          $nb = max($nb, $this->NbLines($current_col[$b]['width'], $current_col[$b]['text']));            
+          $height = $current_col[$b]['height'];
+        }  
+        $h=$height*$nb;
+        
+        
+        // Issue a page break first if needed
+        $this->CheckPageBreak($h);
+        
+        // Draw the cells of the row
+        for($b = 0; $b < sizeof($current_col); $b++)
+        {
+          $w = $current_col[$b]['width'];
+          $a = $current_col[$b]['align'];
+          
+          // Save the current position
+          $x=$this->GetX();
+          $y=$this->GetY();
+          
+          // set style
+          $this->SetFont($current_col[$b]['font_name'], $current_col[$b]['font_style'], $current_col[$b]['font_size']);
+          $color = explode(",", $current_col[$b]['fillcolor']);
+          $this->SetFillColor($color[0], $color[1], $color[2]);
+          $color = explode(",", $current_col[$b]['textcolor']);
+          $this->SetTextColor($color[0], $color[1], $color[2]);            
+          $color = explode(",", $current_col[$b]['drawcolor']);            
+          $this->SetDrawColor($color[0], $color[1], $color[2]);
+          $this->SetLineWidth($current_col[$b]['linewidth']);
+          
+          $color = explode(",", $current_col[$b]['fillcolor']);            
+          $this->SetDrawColor($color[0], $color[1], $color[2]);
+          
+          
+          // Draw Cell Background
+          $this->Rect($x, $y, $w, $h, 'FD');
+          
+          $color = explode(",", $current_col[$b]['drawcolor']);            
+          $this->SetDrawColor($color[0], $color[1], $color[2]);
+          
+          // Draw Cell Border
+          if (substr_count($current_col[$b]['linearea'], "T") > 0)
+          {
+              $this->Line($x, $y, $x+$w, $y);
+          }            
+          
+          if (substr_count($current_col[$b]['linearea'], "B") > 0)
+          {
+              $this->Line($x, $y+$h, $x+$w, $y+$h);
+          }            
+          
+          if (substr_count($current_col[$b]['linearea'], "L") > 0)
+          {
+              $this->Line($x, $y, $x, $y+$h);
+          }
+                      
+          if (substr_count($current_col[$b]['linearea'], "R") > 0)
+          {
+              $this->Line($x+$w, $y, $x+$w, $y+$h);
+          }
+          
+          
+          // Print the text
+          $this->MultiCell($w, $current_col[$b]['height'], $current_col[$b]['text'], 0, $a, 0);
+          
+          // Put the position to the right of the cell
+          $this->SetXY($x+$w, $y);         
+        }
+        
+        // Go to the next line
+        $this->Ln($h);          
+    }               
+  }
+
 function SetMargins($left, $top, $right=null)
 {
 	// Set left, top and right margins
